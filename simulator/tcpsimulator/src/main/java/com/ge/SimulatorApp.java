7169d8b4c865dd39f2152cfc8df260d0c38f54e9
Nuthan Kumar <nuthankumar@villa.local>
2022-01-19 00:04:12 +0530

    changes for code refactor

--- simulator/tcpsimulator/src/main/java/com/ge/SimulatorApp.java
+++ simulator/tcpsimulator/src/main/java/com/ge/SimulatorApp.java
@@ -2,7 +2,7 @@ package com.ge;
 
 import io.vertx.core.Vertx;
 
-public class Main {
+public class SimulatorApp {
     public static void main(String[] args) {
         Vertx vertx =Vertx.vertx();
         vertx.deployVerticle(new SimulatorVerticle());
