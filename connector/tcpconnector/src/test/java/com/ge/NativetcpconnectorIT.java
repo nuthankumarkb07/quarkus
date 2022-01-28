0b240e56f243f262e39ab27b812d1ff5b018703f
Nuthan Kumar <nuthankumar@villa.local>
2022-01-29 02:01:18 +0530

    clean up

--- connector/tcpconnector/src/test/java/com/ge/NativetcpconnectorIT.java
+++
@@ -1,9 +0,0 @@
-package com.ge;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativetcpconnectorIT extends tcpconnectorTest {
-
-    // Execute the same tests but in native mode.
-}
