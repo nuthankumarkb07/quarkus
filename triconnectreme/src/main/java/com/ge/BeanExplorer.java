dd37868cab3507fad121261f0e27eb3c8a07f7d6
Nuthan Kumar <nuthankumar@villa.local>
2022-01-30 23:00:33 +0530

    bean manager

--- triconnectreme/src/main/java/com/ge/BeanExplorer.java
+++ triconnectreme/src/main/java/com/ge/BeanExplorer.java
@@ -9,16 +9,22 @@ import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Inject;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
 @Any
 @ApplicationScoped
 public class BeanExplorer {
     @Inject BeanManager beanManager;
+    private static final Logger logger = LoggerFactory.getLogger(BeanExplorer.class);
     public void getbeans(BeanManager beanManager){
         final String SPACE = " ";
         final AtomicInteger counter = new AtomicInteger();
         final Set<Bean<?>> beans = beanManager.getBeans(Object.class, new AnnotationLiteral<Any>() {});
         for (final Bean<?> bean : beans) {
-            System.out.println(counter.getAndIncrement() + SPACE + bean.getBeanClass().getName());
+            if (bean.getBeanClass().getName().contains("com.ge")) {
+                logger.info(counter.getAndIncrement() + SPACE + bean.getBeanClass().getName());
+            }
         }
     }
 }
